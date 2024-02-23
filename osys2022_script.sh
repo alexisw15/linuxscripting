9cc1a910c644b9a044df3f4f3de83faa1f5e0b30
alexisw15 <W0426652@campus.nscc.ca>
2024-02-23 12:22:12 -0400

    partially written

--- osys2022_script.sh
+++ osys2022_script.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
-echo "kjfgjf"
+lsmnts(){
+    findmnt -D -t notmpfs, nodevtmpfs
+}
+
+echo "Welcome!"
+echo ""
+echo "_____ Kernel Name, Release, Version _____"
+uname -a
+echo ""
+echo "_____ Memory usage stats: _____"
+free -h
+echo "_____ Disk usage stats: _____"
+lsmnts
