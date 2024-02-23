2f98d839cfcdd934765a4806e073d3c3dd1954e4
alexisw15 <W0426652@campus.nscc.ca>
2024-02-23 13:59:31 -0400

    getting there, mostly functional

--- osys2022_script.sh
+++ osys2022_script.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
+{
+#generates unique log file name with time stamp
+#generate_filename() {
+#    date +"log_%Y%m%d_%H%M%S.txt"
+#}
+
+#this is supposed to filter to just "real" disks but doesnt work
 lsmnts(){
-    findmnt -D -t notmpfs, nodevtmpfs
+    findmnt -D #-t notmpfs, nodevtmpfs
 }
 
 echo "Welcome!"
-echo ""
+echo "It is currently:" 
+date --iso-8601=seconds
 echo "_____ Kernel Name, Release, Version _____"
 uname -a
 echo ""
 echo "_____ Memory usage stats: _____"
 free -h
+echo ""
 echo "_____ Disk usage stats: _____"
 lsmnts
+echo ""
+echo "_____ Running processes info: _____ "
+ps aux --sort -rss | head -n 5
+} 2>&1 | tee log.txt
