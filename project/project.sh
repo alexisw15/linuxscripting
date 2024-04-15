8436e37c665be1948cfc36bc49e74bc0a11f597c
alexisw15 <w0426652@campus.nscc.ca>
2024-04-15 10:09:38 -0300

    its still effed

--- project/project.sh
+++ project/project.sh
@@ -4,11 +4,6 @@
 
 
 
-
-
-echo "Welcome to the file backup utility"
-#!/bin/bash
-
 echo "Welcome to the file backup utility"
 
 while true; do
@@ -17,13 +12,13 @@ while true; do
     echo "2) Choose compression options"
     echo "3) Start backup"
     echo "4) Exit"
-    read -p "Enter your choice: " choice
+    read -r -p "Enter your choice: " choice
 
     case $choice in
         1)
             echo "Choose backup location (e.g., /backups/, anywhere not your home directory) (backup directory will be created in this path)"
             while true; do
-                read -p "Enter path: " backup_location #reads user input, in this case a path to a directory
+                read -r -p "Enter path: " backup_location #reads user input, in this case a path to a directory
                 # Validate user input
                 if [[ -d "$backup_location" ]]; then #checks if location entered by user is a valid directory
                     echo "Backup location set to: $backup_location" #uses location variable to confirm to the user the location they entered
@@ -34,24 +29,31 @@ while true; do
             done
             ;;
         2)
-            read -p "Compress files (y/N)? "
+            read -r -p "Compress files (y/N)? "
             case "$compress_choice" in
-            [Yy]) compress_choice=true;;
-            [Nn]) compress_choice=false;;
+            [Yy]) compress_choice=1;;
+            [Nn]) compress_choice=0;;
             esac
             echo "you chose $compress_choice"
             ;;
         3)
 
-            mkdir "$backup_location/backup"
+            mkdir "$backup_location/backup" #create target directory
+            
+            echo "Starting backup..."
+            
+            cp -r "/home/$USER" "/$backup_location/backup" #cp -r copies files recursively ie all files in a directory, files within those directories
+            
+            tar -cvzf backupCompressed "/$backup_location/backup" #tar stores files in an archive -v verbose, show output -z compress with gzip -f specifies file name
 
-            cp -r "/home/$USER" "/$backup_location/backup"
+
+            
             
             sleep 5
             
             
             
-            echo "Starting backup..."
+            
             
             echo "Backup completed."
             ;;
